853eadbfa507902a4a3621fb3622c90de6edbdac
Pedro Goncalves <pedrogoncalvescm@gmail.com>
2023-06-26 14:03:25 -0300

    Criação dos Controllers+atualização das migrations

--- Biblioteca Chat GPT/database/migrations/1687467091085_criar_livros.ts
+++ Biblioteca Chat GPT/database/migrations/1687467091085_criar_livros.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments()
-      table.string('nome').notNullable()
+      table.string('titulo').notNullable()
       table.string('autor').notNullable()
       table.string('descricao').notNullable()
       table.integer('biblioteca_id').unsigned().references('id').inTable('bibliotecas').onDelete('CASCADE')
